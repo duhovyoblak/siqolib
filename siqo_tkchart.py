43f285eb5a7bd29a71f776cb1af28bdf5c90fdbf
Pavol Horanský <duhovyoblak@gmail.com>
2023-02-12 14:50:40 +0100

    siqo_tkchart.py

--- siqo_tkchart.py
+++ siqo_tkchart.py
@@ -41,11 +41,13 @@ class SiqoChart(FigureCanvasTkAgg):
     def __init__(self, journal, name, container, **kwargs):
         "Call constructor of SiqoChart and initialise it"
 
+        journal.I(f'{name}.init:')
+
         self.journal = journal
         self.name    = name
-        self.journal.I(f'{self.name}.init:')
-        self.w = 400
-        self.h = 200
+        self.dat     = None
+        self.w       = 600
+        self.h       = 400
         
         self.fig = plt.figure(figsize=(self.w*_FIG_W/100, self.h*_FIG_H/100), dpi=_DPI)
 
@@ -55,17 +57,23 @@ class SiqoChart(FigureCanvasTkAgg):
         super().__init__(self.fig, master=container)
 
         self.navBar = NavigationToolbar2Tk(self, container)
-
         self.get_tk_widget().place(x=self.w*0.0, y=self.h*0.0)
         
+
+        self.chart = self.fig.add_subplot()
+        self.chart.set_title("Left", fontsize=14)
+        self.chart.grid(False)
+        self.chart.set_facecolor('yellow')
+        self.chart.set_xlabel( 'X0' )
+        self.chart.set_ylabel( 'X1')
+
+
 
         #----------------------------------------------------------------------
         # Internal objects
         #----------------------------------------------------------------------
         self.clear()
         
-        self.show()
-
 
         #----------------------------------------------------------------------
         # Bind events on this TreeView to respective methods
@@ -79,16 +87,39 @@ class SiqoChart(FigureCanvasTkAgg):
         
         pass
     
+    #--------------------------------------------------------------------------
+    def setData(self, dat):
+        "Clears data and set new data"
+        
+        self.dat = dat
+        
     #==========================================================================
     # Show the chart
     #--------------------------------------------------------------------------
     def show(self):
+
+        self.journal.I(f'{self.name}.show:')
         
+        dat = self.dat.getData()
+        X = dat['x1']
+        Y = dat['x2']
+        C = dat['re']
+
+        print(X)
+        print(Y)
+        print(C)
         
+        sctrObj = self.chart.scatter( x=X, y=Y, c=C )
+#        sctrObj = self.chart.scatter( x=dat['x1'], y=dat['x2'], c=dat['re'], marker="s", lw=0, s=(72./self.fig.dpi)**2, cmap='RdYlBu_r')
+        self.fig.colorbar(sctrObj, ax=self.chart)
+            
+
+        # Vykreslenie noveho grafu
+        self.fig.tight_layout()
+#        self.update()
+        self.draw()
         
-        pass
-#        self.draw()
-        
+        self.journal.O()
         
         
     #--------------------------------------------------------------------------
@@ -107,6 +138,34 @@ class SiqoChart(FigureCanvasTkAgg):
         else:
             print('Clicked ouside axes bounds but inside plot window')
     
+        
+    #==========================================================================
+    # Tools for figure setting
+    #--------------------------------------------------------------------------
+    def getDataLabel(self, key):
+        "Return data label for given data's key"
+        
+        return "${}$ [{}{}]".format(key, self.meta[key]['unit'], 
+                                         self.meta[key]['dim' ])
+    
+    #--------------------------------------------------------------------------
+    def getValByGrid(self, gv, key):
+        "Return rescaled value for given grid's value and data's key"
+        
+        gl = self.meta['g'+key]['max'] - self.meta['g'+key]['min']
+        vl = self.meta[    key]['max'] - self.meta[    key]['min']
+        
+        return (gv/gl) * vl * self.meta[key]['coeff']
+    
+    #--------------------------------------------------------------------------
+    def getObjScatter(self):
+        "Returns plotable data for Object value"
+        
+        self.journal.M( f"{self.name}.getObjScatter" )
+
+#        return lib.squareData(baseObj=self.obj, vec=self.obj.prtLst)
+#        return lib.spiralData(baseObj=self.obj, vec=self.obj.prtLst)
+        
         
         
 
